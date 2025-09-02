e10485c5454b8f3729eaa1e242514c529c81e9f4
Fabio Meier <fabio.meier@sensirion.com>
2025-09-02 11:09:09 +0200

    feat(nvim): <leader>y etc keymaps to interact to system clipboard

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,23 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("i", "jj", "<ESC>", { silent = true })
+-- lua/plugins/keymaps.lua
+return {
+  "LazyVim/LazyVim",
+  opts = {
+    keys = {
+      -- Your existing mapping, converted to the LazyVim format.
+      -- I also corrected a small typo ("jj," to "jj").
+      { "jj", "<Esc>", mode = "i", desc = "Exit insert mode" },
+
+      -- Yank/Copy to system clipboard
+      { "<leader>y", '"+y', mode = { "n", "v" }, desc = "Yank to clipboard" },
+      { "<leader>yy", '"+yy', mode = "n", desc = "Yank line to clipboard" },
+      { "<leader>Y", '"+yg_', mode = "n", desc = "Yank to line end to clipboard" },
+
+      -- Paste from system clipboard
+      { "<leader>p", '"+p', mode = { "n", "v" }, desc = "Paste from clipboard" },
+      { "<leader>P", '"+P', mode = { "n", "v" }, desc = "Paste from clipboard (before)" },
+    },
+  },
+}
