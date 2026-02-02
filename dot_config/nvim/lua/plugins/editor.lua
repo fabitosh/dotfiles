e1be7b26eb3ecfc28f8037b295503a8a5d427451
Fabio Meier <fabio.meier@sensirion.com>
2026-02-02 11:12:02 +0100

    fix(nvim): import order. json should not load in plugins

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -89,7 +89,4 @@ return {
       })
     end,
   },
-
-  -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
-  { import = "lazyvim.plugins.extras.lang.json" },
 }
