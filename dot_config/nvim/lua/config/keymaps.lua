a88a6e97d1fffb2c48eec5370b8b1cbcc39395fd
Fabio Meier <fabitosh@gmail.com>
2026-05-30 00:25:05 +0200

    feat(nvim): keybindings

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,23 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
--- lua/plugins/keymaps.lua
-return {
-  "LazyVim/LazyVim",
-  opts = {
-    keys = {
-      -- Your existing mapping, converted to the LazyVim format.
-      -- I also corrected a small typo ("jj," to "jj").
-      { "jj", "<Esc>", mode = "i", desc = "Exit insert mode" },
 
-      -- Yank/Copy to system clipboard
-      { "<leader>y", '"+y', mode = { "n", "v" }, desc = "Yank to clipboard" },
-      { "<leader>yy", '"+yy', mode = "n", desc = "Yank line to clipboard" },
-      { "<leader>Y", '"+yg_', mode = "n", desc = "Yank to line end to clipboard" },
+local map = vim.keymap.set
 
-      -- Paste from system clipboard
-      { "<leader>p", '"+p', mode = { "n", "v" }, desc = "Paste from clipboard" },
-      { "<leader>P", '"+P', mode = { "n", "v" }, desc = "Paste from clipboard (before)" },
-    },
-  },
-}
+-- Exit insert mode
+map("i", "jj", "<Esc>", { desc = "Exit insert mode" })
+
+-- Yank/Copy to system clipboard
+map({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to clipboard" })
+map("n", "<leader>yy", '"+yy', { desc = "Yank line to clipboard" })
+map("n", "<leader>Y", '"+yg_', { desc = "Yank to line end to clipboard" })
+
+-- Paste from system clipboard
+map({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from clipboard" })
+map({ "n", "v" }, "<leader>P", '"+P', { desc = "Paste from clipboard (before)" })
